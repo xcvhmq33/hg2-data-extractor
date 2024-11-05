880f7e6ac4721712936b5b5e0ef8fed389c460da
xcvhmq33 <98145397+xcvhmq33@users.noreply.github.com>
2024-11-05 18:28:15 +0300

    :art: Rename rewrite to overwrite

--- src/hg2_data_extractor/cli.py
+++ src/hg2_data_extractor/cli.py
@@ -10,14 +10,14 @@ from .data_extractor import DataExtractor
 app = typer.Typer()
 
 
-def ask_for_rewrite_if_exists(file_path: str) -> bool:
+def ask_for_overwrite_if_exists(file_path: str) -> bool:
     if Path(file_path).is_file():
         message = f"{file_path} is already exists, overwrite it? (y/n): "
-        rewrite = input(message).lower() == "y"
+        overwrite = input(message).lower() == "y"
     else:
-        rewrite = True
+        overwrite = True
 
-    return rewrite
+    return overwrite
 
 
 @app.command(help="Extracts all asset names from data_all_dec.unity3d")
@@ -28,7 +28,7 @@ def lst(
     ] = "data_all/data_all_dec.unity3d",
 ) -> None:
     data_extractor = DataExtractor(data_all_file_path)
-    if ask_for_rewrite_if_exists(output_file_path):
+    if ask_for_overwrite_if_exists(output_file_path):
         data_extractor.extract_asset_names(output_file_path)
 
 
@@ -39,7 +39,7 @@ def download(
     output_dir_path: Annotated[str, typer.Argument()] = "data_all",
 ) -> None:
     data_downloader = DataDownloader(server, version)
-    if ask_for_rewrite_if_exists(f"{output_dir_path}/data_all.unity3d"):
+    if ask_for_overwrite_if_exists(f"{output_dir_path}/data_all.unity3d"):
         data_downloader.download_data_all(output_dir_path)
 
 
@@ -50,7 +50,7 @@ def decrypt(
         str, typer.Argument()
     ] = "data_all/data_all_dec.unity3d",
 ) -> None:
-    if ask_for_rewrite_if_exists(output_file_path):
+    if ask_for_overwrite_if_exists(output_file_path):
         DataCipher.decrypt_file(input_file_path, output_file_path)
 
 
@@ -68,7 +68,7 @@ def extract(
     data_extractor = DataExtractor(data_all_file_path)
     for asset_name in asset_names.split(","):
         extract_file_path = f"{output_dir_path}/{asset_name}.tsv"
-        if ask_for_rewrite_if_exists(extract_file_path):
+        if ask_for_overwrite_if_exists(extract_file_path):
             data_extractor.extract_asset(asset_name, output_dir_path)
 
 
