a60d0fc049f8732b686f036e5d023172533349a6
xcvhmq33 <98145397+xcvhmq33@users.noreply.github.com>
2024-11-20 02:13:03 +0300

    :boom: Major CLI changes - :boom: Replace a lot of arguments with options - :children_crossing: Add user-friendly error-handling - :sparkles: Add extracting from file support - :sparkles: Add extracting by preset support - :recycle: Add enums for preset data - :art: Simplify naming - :art: Move help from app.command to docstring

--- src/hg2_data_extractor/cli.py
+++ src/hg2_data_extractor/cli.py
@@ -1,3 +1,4 @@
+import functools
 from pathlib import Path
 from typing import Annotated
 
@@ -6,57 +7,157 @@ import typer
 from .data_cipher import DataCipher
 from .data_downloader import DataDownloader
 from .data_extractor import DataExtractor
-from .enums import Server
+from .enums import PRESETS, Preset, Server
 
 app = typer.Typer()
 
+output_dir_option = typer.Option(
+    "--output-dir",
+    "-o",
+    help="Path to the directory where files will be saved.",
+)
 
-@app.command(help="Extracts all asset names from data_all_dec.unity3d")
+data_all_option = typer.Option(
+    "--data-all",
+    "-d",
+    help="Path to the data_all_dec.unity3d file.",
+)
+
+
+def handle_errors(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            typer.secho(e, fg=typer.colors.RED, err=True)
+            raise typer.Exit(code=1)
+
+    return wrapper
+
+
+@app.command()
+@handle_errors
 def lst(
-    output_file_path: Annotated[Path, typer.Argument()] = Path(
-        "extracted/asset_names.txt"
-    ),
-    data_all_file_path: Annotated[Path, typer.Argument()] = Path(
-        "data_all/data_all_dec.unity3d"
-    ),
+    output_dir: Annotated[
+        Path,
+        output_dir_option,
+    ] = Path("extracted"),
+    data_all: Annotated[
+        Path,
+        data_all_option,
+    ] = Path("data_all/data_all_dec.unity3d"),
 ) -> None:
-    data_extractor = DataExtractor(data_all_file_path)
-    data_extractor.list_asset_names(output_file_path)
+    """
+    Writes names list of all existed assets.
+    """
+    output = output_dir / "lst.txt"
+    data_extractor = DataExtractor(data_all)
+    data_extractor.list_asset_names(output)
 
 
-@app.command(help="Downloads data_all.unity3d from server")
+@app.command()
+@handle_errors
 def download(
-    server: Server,
-    version: str,
-    output_dir_path: Annotated[Path, typer.Argument()] = Path("data_all"),
+    server: Annotated[Server, typer.Argument(case_sensitive=False)],
+    version: Annotated[str, typer.Argument()],
+    decrypt_: Annotated[
+        bool,
+        typer.Option(
+            "--decrypt",
+            help="Decrypts the downloaded data.",
+        ),
+    ] = False,
+    output_dir: Annotated[
+        Path,
+        output_dir_option,
+    ] = Path("data_all"),
 ) -> None:
+    """
+    Downloads data_all.unity3d
+    """
     data_downloader = DataDownloader(server, version)
-    data_downloader.download_data_all(output_dir_path, progressbar=True)
+    data_downloader.download_data_all(output_dir, progressbar=True)
+    if decrypt_:
+        output = output_dir / "data_all.unity3d"
+        decrypt(
+            input=output,
+            output_dir=output_dir,
+        )
 
 
-@app.command(help="Decrypts data_all.unity3d")
+@app.command()
+@handle_errors
 def decrypt(
-    input_file_path: Annotated[Path, typer.Argument()] = Path(
-        "data_all/data_all.unity3d"
-    ),
-    output_path: Annotated[Path, typer.Argument()] = Path("data_all"),
+    input: Annotated[
+        Path,
+        typer.Argument(
+            help="Path to the file that will be decrypted.",
+        ),
+    ] = Path("data_all/data_all.unity3d"),
+    output_dir: Annotated[
+        Path,
+        output_dir_option,
+    ] = Path("data_all"),
 ) -> None:
-    DataCipher.decrypt_file(input_file_path, output_path)
+    """
+    Decrypts data_all.unity3d.
+    """
+    DataCipher.decrypt_file(input, output_dir)
 
 
-@app.command(help="Extracts text assets data from data_all_dec.unity3d")
+@app.command()
+@handle_errors
 def extract(
     asset_names: Annotated[
-        str, typer.Argument()
-    ] = "WeaponDataV3,CostumeDataV2,PassiveSkillDataV3,SpecialAttributeDataV2,PetData,PetSkillData",
-    output_dir_path: Annotated[Path, typer.Argument()] = Path("extracted"),
-    data_all_file_path: Annotated[Path, typer.Argument()] = Path(
-        "data_all/data_all_dec.unity3d"
-    ),
+        list[str],
+        typer.Argument(help="List of asset names that will be extraced."),
+    ] = None,
+    asset_file: Annotated[
+        Path,
+        typer.Option(
+            "--asset-file",
+            "-f",
+            help="Path to the file with asset names. Overrides manual input.",
+        ),
+    ] = None,
+    preset: Annotated[
+        Preset,
+        typer.Option(
+            "--preset",
+            "-p",
+            help="Preset of assets to extract. Overrides manual and file input.",
+        ),
+    ] = None,
+    output_dir: Annotated[
+        Path,
+        output_dir_option,
+    ] = Path("extracted"),
+    data_all: Annotated[
+        Path,
+        data_all_option,
+    ] = Path("data_all/data_all_dec.unity3d"),
 ) -> None:
-    data_extractor = DataExtractor(data_all_file_path)
-    for asset_name in asset_names.split(","):
-        data_extractor.extract_asset(asset_name, output_dir_path)
+    """
+    Extracts provided assets.
+    """
+
+    if preset:
+        asset_names = PRESETS[preset]
+    elif asset_file:
+        with asset_file.open(encoding="utf-8") as file:
+            asset_names = [line.strip() for line in file if line.strip()]
+    elif not asset_names:
+        typer.secho(
+            "You must provide either a list of asset names, or use --asset-file[-f], or use --preset[-p].",
+            fg=typer.colors.RED,
+            err=True,
+        )
+        raise typer.Exit(code=1)
+
+    data_extractor = DataExtractor(data_all)
+    for asset_name in asset_names:
+        data_extractor.extract_asset(asset_name, output_dir)
 
 
 def main() -> None:
--- src/hg2_data_extractor/enums.py
+++ src/hg2_data_extractor/enums.py
@@ -4,3 +4,39 @@ from enum import Enum
 class Server(Enum):
     JP = "JP"
     CN = "CN"
+
+
+class Preset(Enum):
+    ITEMS = "items"
+    STORY = "story"
+
+
+class ItemData(Enum):
+    WEAPON = "WeaponDataV3"
+    COSTUME = "CostumeDataV2"
+    BADGE = "PassiveSkillDataV3"
+    SKILL = "SpecialAttributeDataV2"
+    PET = "PetData"
+    PET_SKILL = "PetSkillData"
+
+
+class StoryData(Enum):
+    PLAYBACK = "PlayBackStoryData"
+    PLAYBACK_TITLE = "PlayBackStoryTitleData"
+    EXT = "StoryExtData"
+    FIGUE = "StoryFigueSettingData"
+    MAIN = "StoryMainData"
+    SUB = "StorySubData"
+    CHOICE = "StoryChoiceData"
+    PARTNER = "PartnerStoryHeadData"
+    PARTNER_POSTER = "PartnerPosterData"
+    STORY = "StoryDataV2"
+    LIBRARY = "LibraryData"
+    LIBRARY_KEYWORD = "LibraryKeyWordData"
+    DLC = "DLCStory"
+
+
+PRESETS: dict[Preset, list[str]] = {
+    Preset.ITEMS: [item.value for item in ItemData],
+    Preset.STORY: [story.value for story in StoryData],
+}
