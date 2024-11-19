bee6387d828fbfade84dd3afc3ad30924cdcb1d0
xcvhmq33 <98145397+xcvhmq33@users.noreply.github.com>
2024-11-20 02:00:16 +0300

    :art: Open closed functions in DataDownloader

--- src/hg2_data_extractor/data_downloader.py
+++ src/hg2_data_extractor/data_downloader.py
@@ -14,18 +14,18 @@ from .enums import Server
 class DataDownloader:
     def __init__(self, server: Server, version: str):
         self.server = server
-        self._validate_version(version)
+        self.validate_version(version)
         self.version = version.replace(".", "_").strip()
-        self.data_url = self._get_data_url()
+        self.data_url = self.get_data_url()
 
     def download_data_all(
         self, output_dir_path: Path, progressbar: bool = False
     ) -> None:
         output_dir_path.mkdir(parents=True, exist_ok=True)
         output_file_path = output_dir_path / "data_all.unity3d"
-        data_version_file = self._get_data_version_file()
-        data_json = self._parse_data_json(data_version_file)
-        data_all_name = self._parse_data_all_name(data_json)
+        data_version_file = self.get_data_version_file()
+        data_json = self.parse_data_json(data_version_file)
+        data_all_name = self.parse_data_all_name(data_json)
         data_all_url = f"{self.data_url}/AssetBundles/{data_all_name}"
         response = requests.get(data_all_url, stream=True)
         response.raise_for_status()
@@ -47,7 +47,7 @@ class DataDownloader:
                     t.update(len(chunk))
                     f.write(chunk)
 
-    def _get_data_url(self) -> str:
+    def get_data_url(self) -> str:
         data_urls = {
             Server.CN: f"https://assets.hsod2.benghuai.com/asset_bundle/{self.version}/original/android/Data",
             Server.JP: f"https://s3-ap-northeast-1.amazonaws.com/hsod2-asset/asset_bundle/{self.version}/jporiginal/android/Data",
@@ -55,7 +55,7 @@ class DataDownloader:
 
         return data_urls[self.server]
 
-    def _get_data_version_file(self) -> bytes:
+    def get_data_version_file(self) -> bytes:
         data_version_url = f"{self.data_url}/DataVersion.unity3d"
         response = requests.get(data_version_url)
         if response.status_code == 403:
@@ -64,7 +64,7 @@ class DataDownloader:
 
         return response.content
 
-    def _parse_data_json(self, data_version_file: bytes) -> str:
+    def parse_data_json(self, data_version_file: bytes) -> str:
         bundle = UnityPy.load(data_version_file)
         asset_reader = bundle.objects[1]
         asset: TextAsset = asset_reader.read()
@@ -72,7 +72,7 @@ class DataDownloader:
 
         return data_json
 
-    def _parse_data_all_name(self, data_json: str) -> str:
+    def parse_data_all_name(self, data_json: str) -> str:
         parsed_data_json: dict[str, str] = json.loads(data_json)
         n = parsed_data_json["N"]
         hs = parsed_data_json["HS"]
@@ -82,7 +82,7 @@ class DataDownloader:
         return data_all_name
 
     @staticmethod
-    def _validate_version(version: str) -> None:
+    def validate_version(version: str) -> None:
         if not re.match(r"^(?:[1-9]|[1-9]\d)[.|_][0-9]$", version):
             msg = "Version must be a valid game version (e.g `11_1` or `9.8`)"
             raise ValueError(msg)
