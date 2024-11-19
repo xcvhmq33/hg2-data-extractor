afc900d233c781e479ddf5dfd8de245d4fcf6318
xcvhmq33 <98145397+xcvhmq33@users.noreply.github.com>
2024-11-20 00:59:56 +0300

    :fire: Remove asking for overwriting

--- src/hg2_data_extractor/cli.py
+++ src/hg2_data_extractor/cli.py
@@ -6,7 +6,6 @@ from .data_cipher import DataCipher
 from .data_downloader import DataDownloader
 from .data_extractor import DataExtractor
 from .enums import Server
-from .utils import ask_overwrite_if_exists
 
 app = typer.Typer()
 
@@ -18,9 +17,8 @@ def lst(
         str, typer.Argument()
     ] = "data_all/data_all_dec.unity3d",
 ) -> None:
-    if ask_overwrite_if_exists(output_file_path):
-        data_extractor = DataExtractor(data_all_file_path)
-        data_extractor.extract_asset_names(output_file_path)
+    data_extractor = DataExtractor(data_all_file_path)
+    data_extractor.extract_asset_names(output_file_path)
 
 
 @app.command(help="Downloads data_all.unity3d from server")
@@ -29,10 +27,8 @@ def download(
     version: str,
     output_dir_path: Annotated[str, typer.Argument()] = "data_all",
 ) -> None:
-    output_file_path = f"{output_dir_path}/data_all.unity3d"
-    if ask_overwrite_if_exists(output_file_path):
-        data_downloader = DataDownloader(server, version)
-        data_downloader.download_data_all(output_dir_path, progressbar=True)
+    data_downloader = DataDownloader(server, version)
+    data_downloader.download_data_all(output_dir_path, progressbar=True)
 
 
 @app.command(help="Decrypts data_all.unity3d")
@@ -42,8 +38,7 @@ def decrypt(
         str, typer.Argument()
     ] = "data_all/data_all_dec.unity3d",
 ) -> None:
-    if ask_overwrite_if_exists(output_file_path):
-        DataCipher.decrypt_file(input_file_path, output_file_path)
+    DataCipher.decrypt_file(input_file_path, output_file_path)
 
 
 @app.command(help="Extracts text assets data from data_all_dec.unity3d")
@@ -58,9 +53,7 @@ def extract(
 ) -> None:
     data_extractor = DataExtractor(data_all_file_path)
     for asset_name in asset_names.split(","):
-        extract_file_path = f"{output_dir_path}/{asset_name}.tsv"
-        if ask_overwrite_if_exists(extract_file_path):
-            data_extractor.extract_asset(asset_name, output_dir_path)
+        data_extractor.extract_asset(asset_name, output_dir_path)
 
 
 def main() -> None:
